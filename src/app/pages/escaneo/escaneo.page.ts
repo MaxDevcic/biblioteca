c37fdac4b33080adf05b12d783303d1115c1cf8d
unknown <ma.devcic@duocuc.cl>
2025-01-27 10:11:44 -0300

    codigo de barra

--- src/app/pages/escaneo/escaneo.page.ts
+++ src/app/pages/escaneo/escaneo.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-escaneo',
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class EscaneoPage implements OnInit {
+  isSupported = false;
+  barcodes: Barcode[] = [];
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
+  ngOnInit(){
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
 
-  ngOnInit() {
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permiso denegado',
+      message: 'Para usar la aplicación autorizar los permisos de cámara',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
 }
